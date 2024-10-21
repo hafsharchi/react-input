155910ff93ac40d75526ebb24d4d81c2453a7f5e
Hosein Afsharchi <mhafsharchi@gmail.com>
2024-10-21 18:37:11 +0330

    add validation defaults docs

--- input-preview/src/routes/_main-layout/docs/validation-defaults/index.tsx
+++ input-preview/src/routes/_main-layout/docs/validation-defaults/index.tsx
@@ -8,174 +8,74 @@ import { inputConfigs } from "../../../../lib/input_default_settings";
 import { useState } from "react";
 import CodeHighlighter from "../../../../components/CodeHighlighter";
 
-export const Route = createFileRoute("/_main-layout/docs/text copy/")({
-  component: Text,
-});
+export const Route = createFileRoute("/_main-layout/docs/validation-defaults/")(
+  {
+    component: Text,
+  }
+);
 
 function Text() {
-  const { useRegister, submit } = useInput();
   const [activeTab, setActiveTab] = useState<number>(0);
-  const [styled, setStyled] = useState<boolean>(true);
-  const codeSnippet = `import { Input, useInput } from "input-master";
-  
-export const TextInput = () => {
-  const { useRegister, submit } = useInput();
-  
-  return (
-    <>
-      <Input
-        type="text"
-        title="First Name *"
-        required
-        minLength={2}
-        name="firstName"
-        notValidClassName="border !border-rose-500/50"
-        validationComponent={ValidationComponent}
-        validationOn="submit"
-        register={useRegister}
-      />
-      <Input
-        type="text"
-        title="Last Name"
-        name="lastName"
-        validationOn="submit-blur-change"
-        register={useRegister}
-        maxLength={7}
-      />
-      <button
-        onClick={() =>
-          submit((formData) => // automatically checks validation and if is valid:
-            alert("firstName: " + formData.firstName + "lastName: " + formData.lastName)
-          )
-        }
-      >
-        submit
-      </button>
-    </>
-  );
+  const codeSnippet = `import { ReactInputProvider } from "input-master";
+
+const customErrors = {
+  minValue: 'The value is too low. Please enter a higher value.',
+  maxValue: 'The value exceeds the allowed limit. Please enter a lower value.',
+  minLength: 'The input is too short. Please provide more characters.',
+  maxLength: 'The input is too long. Please shorten your input.',
+  required: 'This field is required. Please fill it out.',
 };
+
+return (
+  <ReactInputProvider 
+    errors={customErrors} 
+    onValidationFailedFunction={() => alert('Some fields are invalid. Please check and try again.')}
+  >
+    {/* Your form or other components */}
+  </ReactInputProvider>
+)
+
 `;
   return (
     <>
       <DocsBreadcrumb page="Text Type" />
 
-      <div className="relative  my-4">
-        <div className="flex gap-2">
-          <Button
-            active={activeTab == 0}
-            onClick={() => {
-              setActiveTab(0);
-            }}
-            variant="tab"
-          >
-            Preview
-          </Button>
-          <Button
-            active={activeTab == 1}
-            onClick={() => {
-              setActiveTab(1);
-            }}
-            variant="tab"
-          >
-            Code
-          </Button>
-        </div>
-        <div className="h-[1px] absolute bottom-[1px] bg-foreground opacity-10 w-full"></div>
-      </div>
-      {activeTab == 0 ? (
-        <>
-          <PreviewBox
-            settings={
-              <div className="flex absolute left-4 top-4 gap-1">
-                <Button
-                  active={styled}
-                  onClick={() => {
-                    setStyled(true);
-                  }}
-                >
-                  styled
-                </Button>
-                <Button
-                  active={!styled}
-                  onClick={() => {
-                    setStyled(false);
-                  }}
-                >
-                  default
-                </Button>
-              </div>
-            }
-          >
-            <Input
-              {...inputConfigs(styled)}
-              type="text"
-              title="First Name *"
-              required
-              minLength={2}
-              name="firstName"
-              notValidClassName="border !border-rose-500/50"
-              validationComponent={ValidationComponent}
-              validationOn="submit"
-              register={useRegister}
-            />
-            <Input
-              {...inputConfigs(styled)}
-              type="text"
-              title="Last Name"
-              name="lastName"
-              validationOn="submit-blur-change"
-              register={useRegister}
-              maxLength={7}
-            />
-            <Button variant="submit" className="mx-auto mt-3" onClick={() => submit((d) => alert(`firstName: ${d.firstName}, lastName: ${d.lastName}`))}>Sumbit</Button>
-          </PreviewBox>
-        </>
-      ) : (
-        <>
-          <CodeHighlighter
-            language="tsx"
-            className="text-xs rounded-lg border bg-black shrink-0 h-full w-full"
-            code={codeSnippet}
-          />
-        </>
-      )}
-      <br />
       <h2>Description</h2>
       <p>
-        The Text input type is perfect for capturing single-line text from
-        users, like names or short responses! It automatically includes all the
-        shared features available across our input types. If you're curious
-        about these shared properties, feel free to check them out here.
+        You can customize default validation error messages and handle
+        validation failures across your entire project by wrapping your app or a
+        specific section of it within the<code>ReactInputProvider</code>This
+        allows you to define global behaviors for input validation, including
+        custom error messages for common validation scenarios and a global
+        function to handle validation failures.
       </p>
       <p>
-        Below is a table of specific properties unique to the Text input type:
+        To apply these defaults, you can create a set of custom error messages
+        and pass them, along with other configurations, into the
+        <code>ReactInputProvider</code>. Here’s how you can set it up:
       </p>
-      <h2>Props</h2>
-      <table className="w-full text-center text-sm rounded-lg  border-1 border-red-400">
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Default</th>
-          </tr>
-        </thead>
-        <tbody className="font-light">
-          <tr>
-            <td>MaxLength</td>
-            <td>number</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>MinLength</td>
-            <td>number</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          
-        </tbody>
-      </table>
+      <CodeHighlighter
+        language="tsx"
+        className="text-xs rounded-lg border bg-black shrink-0 h-full w-full"
+        code={codeSnippet}
+      />
+      <br />
+      <h1>Default Error Messages</h1>
+      <p>
+        In the above example, the errors object contains custom messages for
+        common validation errors like<code>minValue</code>,<code>maxValue</code>
+        ,<code>minLength</code>,<code>maxLength</code>, and{" "}
+        <code>required</code>. These error messages will be used by default
+        across all input fields in your project.
+      </p>
+      <br />
+      <h1>Global Validation Failure Handler</h1>
+      <p>
+        The<code>onValidationFailedFunction</code>allows you to define a global behavior
+        when form validation fails. In the example, an alert is triggered when
+        any input field validation fails, but you can replace it with more
+        complex error handling logic, such as logging or custom UI feedback.
+      </p>
     </>
   );
 }
