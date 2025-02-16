52bc280cf8af915e78b21a70b020e3f62c6a8ef7
Hosein Afsharchi <mhafsharchi@gmail.com>
2025-02-16 13:25:01 -0800

    added Table component

--- input-preview/src/routes/_main-layout/docs/text/index.tsx
+++ input-preview/src/routes/_main-layout/docs/text/index.tsx
@@ -7,6 +7,7 @@ import { Button } from "../../../../components/Button";
 import { inputConfigs } from "../../../../lib/input_default_settings";
 import { useState } from "react";
 import CodeHighlighter from "../../../../components/CodeHighlighter";
+import Table from "../../../../components/Table";
 
 export const Route = createFileRoute("/_main-layout/docs/text/")({
   component: Text,
@@ -123,7 +124,17 @@ export const TextInput = () => {
               register={useRegister}
               maxLength={7}
             />
-            <Button variant="submit" className="mx-auto mt-3" onClick={() => submit((d) => alert(`firstName: ${d.firstName}, lastName: ${d.lastName}`))}>Sumbit</Button>
+            <Button
+              variant="submit"
+              className="mx-auto mt-3"
+              onClick={() =>
+                submit((d) =>
+                  alert(`firstName: ${d.firstName}, lastName: ${d.lastName}`)
+                )
+              }
+            >
+              Sumbit
+            </Button>
           </PreviewBox>
         </>
       ) : (
@@ -136,42 +147,17 @@ export const TextInput = () => {
         </>
       )}
       <br />
-      <h2>Description</h2>
-      <p>
-        The Text input type is perfect for capturing single-line text from
-        users, like names or short responses! It automatically includes all the
-        shared features available across our input types. If you're curious
-        about these shared properties, feel free to check them out here.
-      </p>
-      <p>
-        Below is a table of specific properties unique to the Text input type:
-      </p>
+
       <h2>Props</h2>
-      <table className="w-full text-center text-sm rounded-lg  border-1 border-red-400">
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Default</th>
-          </tr>
-        </thead>
-        <tbody className="font-light">
-          <tr>
-            <td>MaxLength</td>
-            <td>number</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>MinLength</td>
-            <td>number</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          
-        </tbody>
-      </table>
+
+      <Table
+        columns={["Prop", "Type", "Description", "Default"]}
+        data={[
+          ["MaxLength", "number", "-", "-"],
+          ["MinLength", "number", "-", "-"],
+        ]}
+      />
+    
     </>
   );
 }
