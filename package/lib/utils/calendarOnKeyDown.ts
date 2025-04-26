c40a23e993fe72a62202efd10cc523d3c8351fa7
hafsharchi <mhafsharchi@gmail.com>
2025-04-26 15:19:48 +0330

    a new date type is coming!

---
+++ package/lib/utils/calendarOnKeyDown.ts
@@ -0,0 +1,61 @@
+export const calendarOnKeyDown = (
+  e: React.KeyboardEvent<HTMLInputElement>,
+  defaultValue: string,
+  firstValue: string,
+  moveNext: boolean = true,
+  movePrevious: boolean = true
+) => {
+  if (e.target instanceof HTMLInputElement)
+    e.target.setSelectionRange(defaultValue.length, defaultValue.length);
+
+  if (e.key === "Delete" || e.key === "Backspace") {
+    e.preventDefault();
+    if (e.target instanceof HTMLInputElement) {
+      e.target.value = defaultValue;
+    }
+  } else if (e.key === "ArrowRight" && moveNext) {
+    e.preventDefault();
+    const nextInput = (e.target as HTMLElement).nextElementSibling
+      ?.nextElementSibling as HTMLInputElement;
+    if (nextInput) {
+      nextInput.focus();
+    }
+  } else if (e.key === "ArrowLeft" && movePrevious) {
+    e.preventDefault();
+    const prevInput = (e.target as HTMLElement).previousElementSibling
+      ?.previousElementSibling as HTMLInputElement;
+    if (prevInput) {
+      prevInput.focus();
+    }
+  } else if (e.key === "ArrowUp") {
+    e.preventDefault();
+    if (e.target instanceof HTMLInputElement) {
+      if (e.target.value === defaultValue) {
+        e.target.value = firstValue.padStart(defaultValue.length, "0");
+      } else {
+        const currentValue = parseInt(e.target.value);
+        if (!isNaN(currentValue)) {
+          e.target.value = String(currentValue + 1).padStart(
+            defaultValue.length,
+            "0"
+          );
+        }
+      }
+    }
+  } else if (e.key === "ArrowDown") {
+    e.preventDefault();
+    if (e.target instanceof HTMLInputElement) {
+      if (e.target.value === defaultValue) {
+        e.target.value = firstValue.padStart(defaultValue.length, "0");
+      } else {
+        const currentValue = parseInt(e.target.value);
+        if (!isNaN(currentValue)) {
+          e.target.value = String(currentValue - 1).padStart(
+            defaultValue.length,
+            "0"
+          );
+        }
+      }
+    }
+  }
+};
--- package/src/App.tsx
+++ package/src/App.tsx
@@ -1,31 +1,105 @@
 // import { Input } from "../lib/components";
 // import { useInput } from "../lib/components/useInput";
-
-import { Input } from "../dist/main";
 import { useInput } from "../dist/main";
+import "./App.css";
+import { calendarOnKeyDown } from "../lib/utils/calendarOnKeyDown";
 
 function App() {
-  const { useRegister, submit } = useInput();
+  const { submit } = useInput();
 
   return (
     <>
       <div>Here we test the component</div>
-      <Input
-        type="select"
-        options={[
-          { label: "Blue", value: "1" },
-          { label: "Red", value: "2" },
-          { label: "Green", value: "3" },
-        ]}
-        required
-        onChange={(e) => console.log(e)}
-        register={useRegister}
-        name="hell"
-      />
+      <DatePicker />
       <div onClick={() => submit((d) => console.log(d))}>submit</div>
       <input type="date" />
     </>
   );
 }
 
+function DatePicker() {
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    defaultValue: string
+  ) => {
+    let value = e.target.value.replaceAll(/[^0-9]/g, "");
+    if (!e.target.value) {
+      e.target.value = defaultValue;
+    } else {
+      if (value.length < defaultValue.length) {
+        value = value.padStart(defaultValue.length, "0");
+      }
+      if (value.length > defaultValue.length) {
+        if (value[0] != "0") {
+          value =
+            "0".repeat(defaultValue.length - 1) + value[defaultValue.length];
+        } else value = value.slice(-defaultValue.length);
+      }
+      e.target.value = value;
+    }
+  };
+  const onBlur = (
+    e: React.FocusEvent<HTMLInputElement>,
+    defaultValue: string,
+    minValue: number,
+    maxValue: number
+  ) => {
+    let value = e.target.value;
+    if (parseInt(value) > maxValue)
+      value = maxValue.toString().padStart(defaultValue.length, "0");
+    if (parseInt(value) < minValue)
+      value = minValue.toString().padStart(defaultValue.length, "0");
+    e.target.value = value;
+  };
+
+  return (
+    <div className="">
+      <input
+        className="input-date"
+        size={1}
+        type="text"
+        onBlur={(e) => onBlur(e, "yyyy", 1, 2030)}
+        onKeyDown={(e) =>
+          calendarOnKeyDown(
+            e,
+            "yyyy",
+            new Date().getFullYear().toString(),
+            true,
+            false
+          )
+        }
+        onChange={(e) => {
+          onChange(e, "yyyy");
+        }}
+        defaultValue={"yyyy"}
+      />
+      <span>/</span>
+      <input
+        className="input-date"
+        size={1}
+        type="text"
+        onKeyDown={(e) =>
+          calendarOnKeyDown(e, "mm", new Date().getUTCMonth().toString())
+        }
+        onChange={(e) => {
+          onChange(e, "mm");
+        }}
+        defaultValue={"mm"}
+      />
+      <span>/</span>
+      <input
+        className="input-date"
+        size={1}
+        type="text"
+        onChange={(e) => {
+          onChange(e, "dd");
+        }}
+        onKeyDown={(e) =>
+          calendarOnKeyDown(e, "dd", new Date().getDay().toString(), false)
+        }
+        defaultValue={"dd"}
+      />
+    </div>
+  );
+}
 export default App;
